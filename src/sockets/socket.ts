2d0bf70eb6546853d71c7e9e1bb5337ae2e9db7f
cristianat98 <cristianarmestotejedo@gmail.com>
2020-12-11 23:14:27 +0100

    Pequeño avance sockets

--- src/sockets/socket.ts
+++ src/sockets/socket.ts
@@ -2,9 +2,14 @@ const { io } = require('../index');
 
 // Mensajes de Sockets
 io.on('connection', (socket: any) => {
-  console.log(socket);
+  //console.log(socket);
   console.log("Nueva conexion");
  
+  socket.on('set-name', (user:any) =>{
+    socket.username = user.username;
+    socket.id = user.id;
+    console.log("El nuevo usuario es: " + socket.username + " con id: " + socket.id);
+  })
   socket.on('disconnect', function(){
       console.log("Desconectado");
     io.emit('users-changed', {user: socket.username, event: 'left'});  
