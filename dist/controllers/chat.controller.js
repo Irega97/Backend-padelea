b7e7d9852b9c62168dc3a7377a65ddf086d774d3
Miquel Puspa Torres Campo <miqueltorrescampo@gmail.com>
2020-12-17 18:30:18 +0100

    'newChat'

--- dist/controllers/chat.controller.js
+++ dist/controllers/chat.controller.js
@@ -1,2 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+function getChat() {
+}
+function getMyChats() {
+}
+function addChat() {
+}
+function addOtroParti() {
+}
+function delChat() {
+}
+exports.default = { getChat, getMyChats, addChat, addOtroParti, delChat };
