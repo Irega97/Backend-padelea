b3dcaf25781bcb2b025aa6e0fe779a30b460ce80
vcarlos97 <vcarlos139@gmail.com>
2020-12-27 02:49:37 +0100

    leave torneo

--- src/routes/torneo.routes.ts
+++ src/routes/torneo.routes.ts
@@ -6,10 +6,12 @@ import torneoController from '../controllers/torneo.controller'
 const router = Router();
 
 router.get('/all', passport.authenticate("jwt", {session: false}), torneoController.getTorneos);
-router.get('/all/me', passport.authenticate("jwt", {session: false}), torneoController.getMyTorneos);
+router.get('/all/:username', passport.authenticate("jwt", {session: false}), torneoController.getTorneosUser);
 router.get('/:name', passport.authenticate("jwt", {session: false}), torneoController.getTorneo);
 
 router.post('/new', passport.authenticate("jwt", {session: false}), torneoController.createTorneo);
+
 router.post('/join/:name', passport.authenticate("jwt", {session: false}), torneoController.joinTorneo);
+router.put('/leave/:name', passport.authenticate("jwt", {session: false}), torneoController.leaveTorneo);
 
 export default router;
