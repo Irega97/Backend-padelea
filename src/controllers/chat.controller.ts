0464831a40aeaba0a94bc2d7eab5f9397eda2a72
Miquel Puspa Torres Campo <miqueltorrescampo@gmail.com>
2020-12-30 00:54:53 +0100

    'delChat'

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -40,13 +40,39 @@ function  addChat(req:Request, res:Response): void {
     })
     
 }
-
-function  addOtroParti(){
+/*
+async function  addOtroParti(req:Request, res:Response): void {
     
+    let c : any;
+
+    await Chat.findOne({"_id": req.params.id}).then((data) => {
+        if(data == null) return res.status(404).json({message: "Chat not found"});
+        else {
+            c = data;
+        }
+    });
+
+    if(req.body.participantes =! null){
+        Chat.updateOne({"_id": req.params.id}, {"users": req.params.participantes}).populate
+    }
+    
+}*/
+
+function  delChat(req:Request, res:Response): void {
+    User.findById(req.user, {chats : 1}).populate({path: 'chats', populate: 
+    {path: 'user', select: 'username'}}).then((data)=>{ 
+        if(data==null) return res.status(404).json();
+        data.chats.forEach(chat => {
+            if(req.params.id == chat._id){
+                User.deleteOne({ idChat: 'idChat' }, function (err) {
+                    return res.status(409).json(err);
+                  });
+                return res.status(200).json(chat);
+            }
+        })
+    }).catch((err) => {
+        return res.status(500).json(err);
+    })
 }
 
-function  delChat(){
-    
-}
-
-export default{getChat, getMyChats, addChat, addOtroParti, delChat}
+export default{getChat, getMyChats, addChat, /*addOtroParti,*/ delChat}
