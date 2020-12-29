093c647c5c7c085ed2701b46f47ee2a9321b4c60
Ivan Requena Garcia <ivanreq97@gmail.com>
2020-12-30 00:56:04 +0100

    jaj

--- dist/controllers/chat.controller.js
+++ dist/controllers/chat.controller.js
@@ -38,8 +38,37 @@ function addChat(req, res) {
         }
     });
 }
-function addOtroParti() {
+/*
+async function  addOtroParti(req:Request, res:Response): void {
+    
+    let c : any;
+
+    await Chat.findOne({"_id": req.params.id}).then((data) => {
+        if(data == null) return res.status(404).json({message: "Chat not found"});
+        else {
+            c = data;
+        }
+    });
+
+    if(req.body.participantes =! null){
+        Chat.updateOne({"_id": req.params.id}, {"users": req.params.participantes}).populate
+    }
+    
+}*/
+function delChat(req, res) {
+    user_1.default.findById(req.user, { chats: 1 }).populate({ path: 'chats', populate: { path: 'user', select: 'username' } }).then((data) => {
+        if (data == null)
+            return res.status(404).json();
+        data.chats.forEach(chat => {
+            if (req.params.id == chat._id) {
+                user_1.default.deleteOne({ idChat: 'idChat' }, function (err) {
+                    return res.status(409).json(err);
+                });
+                return res.status(200).json(chat);
+            }
+        });
+    }).catch((err) => {
+        return res.status(500).json(err);
+    });
 }
-function delChat() {
-}
-exports.default = { getChat, getMyChats, addChat, addOtroParti, delChat };
+exports.default = { getChat, getMyChats, addChat, /*addOtroParti,*/ delChat };
