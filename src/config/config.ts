b0450992b030af10a62ed8142b7407a2402c07ac
manelfraga <manel.etc.etc@gmail.com>
2021-01-15 21:57:47 +0100

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
 //Configuraciones para conexion con BBDD
 export default {
     DB: {
-        URI: process.env.MONGODB_URI || 'mongodb://localhost/proyectoEA',
-        //URI: process.env.MONGODB_URI || 'mongodb://game:gameEA@147.83.7.155:27018/proyectoEA',
+        //URI: process.env.MONGODB_URI || 'mongodb://localhost/proyectoEA',
+        URI: process.env.MONGODB_URI || 'mongodb://game:gameEA@147.83.7.156:27017/bbdd',
     },
     jwtSecret: 'chulibobito',
     defaultImage: 'https://res.cloudinary.com/dyjz5e9a6/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1606479202/default%20images/default_ktlol4.png',
