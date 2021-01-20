4c37f4c47f4fdfae45185156dde4252ec6ca7882
Ivan Requena Garcia <ivanreq97@gmail.com>
2021-01-20 01:28:28 +0100

    resultados hechos

--- src/routes/partido.routes.ts
+++ src/routes/partido.routes.ts
@@ -6,11 +6,7 @@ import partidoController from '../controllers/partido.controller'
 const router = Router();
 
 router.post('/new', passport.authenticate("jwt", {session: false}), partidoController.addPartido);
-<<<<<<< HEAD
 router.get('/:name/:vuelta/:grupo',  passport.authenticate("jwt", {session: false}), partidoController.getInfoGrupos);
-=======
-router.get('/:name/:vuelta/:grupo',  passport.authenticate("jwt", {session: false}), partidoController.getInfoGrupo);
->>>>>>> 0f2127069caa0f9c0c33aea4d40611190e3a918e
 router.post('/results', passport.authenticate("jwt", {session: false}), partidoController.addResultados);
 
 export default router;
