a8550ba9865fb7887c4a5c612cdae688448cb3c6
Ivan Requena Garcia <ivanreq97@gmail.com>
2021-01-26 00:36:18 +0100

    vuelta a empezar

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,9 @@ import app from './app'; //app exportada en app.ts
 //Ejecutamos la conexion a la BBDD antes de escuchar al server
 import './database';
 
-const https = require('https');
-const fs = require('fs');
-
-const options = {
-    key: fs.readFileSync('config/key.pem'),
-    cert: fs.readFileSync('config/cert.pem')
-};
-
-const server = https.createServer(options, app).listen(app.get('port')); //Recuperamos puerto de app.ts
-
-//const server = require('http').createServer(app);
+const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
 require('./sockets/socket');
 
+server.listen(app.get('port')); //Recuperamos puerto de app.ts
 console.log('Server in port', app.get('port'));
